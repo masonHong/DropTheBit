cf96371050e5fe6c93ae346a0c4e0c034a66d395
mason.h <mason.h@kakaocorp.com>
2017-12-15 16:16:51 +0900

    [#1] SplashActivity 추가

---
+++ app/src/main/java/com/dropthebit/dropthebit/ui/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.dropthebit.dropthebit.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dropthebit.dropthebit.R;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Disposable disposable = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        disposable = Observable.just(0)
+                .delay(1000, TimeUnit.MILLISECONDS)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        finish();
+                        startActivity(new Intent(getBaseContext(), MainActivity.class));
+                    }
+                });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        // 백 버튼 시 액티비티 시작 취소
+        if (disposable != null) {
+            disposable.dispose();
+        }
+    }
+}
